a8c852f8114b77eada8bacea10d347cacc406f19
phonhay103 <49369395+phonhay103@users.noreply.github.com>
2021-01-02 20:46:45 +0700

    Update abc4.c

--- HUST/C Basic -- 20191/abc4.c
+++ HUST/C Basic -- 20191/abc4.c
@@ -1,8 +1,3 @@
-/////////////////////////////
-// Họ và tên: Lê Ngọc Long //
-// MSSV     : 20184137     //
-/////////////////////////////
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
